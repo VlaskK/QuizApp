e418fa58dedde8bbd9037426ba5aedae0bc7a46f
vlask <vlad.dondokov@gmail.com>
2021-11-05 14:34:28 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { useState } from 'react';
 import './App.css';
-
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Home from './Pages/Home/Home';
+import Quiz from './Pages/Quiz/Quiz'
+import Result from './Pages/Result/Result'
+import axios from 'axios';
 function App() {
+  const [name, setName] = useState('');
+  const [questions, setQuestions] = useState();
+  const [score, setScore] = useState(0);
+
+
+  const fetchQuestions = async(category='', difficulty='') => {
+    const { data } = await axios.get(
+      `https://opentdb.com/api.php?amount=10${
+        category && `&category=${category}`
+      }${difficulty && `&difficulty=${difficulty}`}&type=multiple`
+    );
+    setQuestions(data.results);
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter >
+      <div className="app" style={{ backgroundImage: "url(./ques1.png)" }}>
+        <Header />
+        <Switch>
+        <Route path='/' exact>
+          <Home name={name} setName={setName} fetchQuestions={fetchQuestions}/>
+        </Route>
+        <Route path="/quiz">
+          <Quiz 
+            name={name}
+            questions={questions}
+            score={score}
+            setScore={setScore}
+            setQuestions={setQuestions}
+            />
+        </Route>
+        <Route path="/result">
+          <Result name={name} score={score}/>
+        </Route>
+      </Switch>
+      </div>
+
+      <Footer />
+
+
+
+    </BrowserRouter>
+
   );
 }
 
